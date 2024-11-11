b2c29891a2a40a508e07a0d6e3fa2ca4092fe829
Fadafranca <fadafranca2@gmail.com>
2024-11-11 20:30:23 -0300

    Adding classes generated from StarUML

---
+++ code/menu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Menu:
+    def __init__(self):
+        self.window = None
+
+    def Operation1(self, ):
+        pass
+
+    def run(self, ):
+        pass
